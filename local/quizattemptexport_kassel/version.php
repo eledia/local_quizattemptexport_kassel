63a61ec5e9474be392fedb2adecc18fc8185667b
Ralf Wiederhold <ralf.wiederhold@eledia.de>
2020-11-27 18:15:48 +0100

    eledia local_quizattemptexport_kassel - Added postprocessing for qtype essay.

--- local/quizattemptexport_kassel/version.php
+++ local/quizattemptexport_kassel/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020112703;
+$plugin->version = 2020112704;
 $plugin->requires  = 2020061501;
 $plugin->component = 'local_quizattemptexport_kassel';
 $plugin->dependencies = array(
