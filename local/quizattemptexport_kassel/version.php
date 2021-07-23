61af81165b3c857226818e17a9a81cb4372f36d0
Ralf Wiederhold <ralf.wiederhold@eledia.de>
2021-07-23 15:17:01 +0200

    eledia local_quizattemptexport_kassel - Added postprocessing for qtype "ddwtos".

--- local/quizattemptexport_kassel/version.php
+++ local/quizattemptexport_kassel/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021060700;
+$plugin->version = 2021072300;
 $plugin->requires  = 2020061501;
 $plugin->component = 'local_quizattemptexport_kassel';
 $plugin->dependencies = array(
